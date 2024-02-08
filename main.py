a86ed6e71a3cca5735fd62789044fc2c0ce72320
vaishnvi shetty <142256509+cherokee22@users.noreply.github.com>
2024-02-08 10:27:43 +0530

    rock papaer sissors
    rock paper sissors

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+# This is a sample Python script.
 import random
 
-number = random.randint(1,100)
-guess = 0
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-while (guess != number):
-     guess = int(input("enter your gugess "))
 
-     if(guess > number ):
-         print(" guess lower")
-     elif(guess < number):
-         print(" guess higher")
-     else:
-      print("you won")
+opt = ("rock","paper","sissors")
+computer = random.choice(opt)
+
+player = input(" enter your choice 'rock' 'paper' 'sissors'")
+
+while player not in opt:
+    print(" enter valid choice")
+    exit()
+print(f":{player}, {computer}")
+
+if(player =="paper" and computer =="sisssors"or"rock"):
+    print(" i win ")
+elif(player == "sissors" or "rock" and computer =="paper"):
+    print(" u won")
+else:
+    print(" tie !!!")
+
 
 
 
