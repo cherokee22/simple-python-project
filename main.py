333ab0ef74977ffb41264f8f0a0825a0e8c4b0a6
vaishnvi shetty <142256509+cherokee22@users.noreply.github.com>
2024-02-15 22:15:30 +0530

    to check whether number is prime or no

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-# This is a sample Python script.
-import random
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-opt = ("rock","paper","sissors")
-computer = random.choice(opt)
-
-player = input(" enter your choice 'rock' 'paper' 'sissors'")
-
-while player not in opt:
-    print(" enter valid choice")
-    exit()
-print(f":{player}, {computer}")
-
-if(player =="paper" and computer =="sisssors"or"rock"):
-    print(" i win ")
-elif(player == "sissors" or "rock" and computer =="paper"):
-    print(" u won")
-else:
-    print(" tie !!!")
-
-
-
-
+num = int(input(" enter the number "))
+i = int(num%2)
+if (i == 0  ):
+    print(" the number is not  prime ")
+else :
+    print(" the number is prime ")
